67e650fe34cad58a970f2d73d3d62f24abd12eab
Vives <vivesrenso@gmail.com>
2023-11-20 15:04:52 -0300

    adicionada linha com versão de aplicação

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine(DateTime.Now.ToLongDateString());
+        Console.WriteLine("Versão da aplicação 1.0");
+    }
+}
